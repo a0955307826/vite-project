5c787301a3f364db456fffc63dd367e4664aea6d
a0955307826 <a0955307826@gmail.com>
2024-06-24 21:12:24 +0800

    新增配置說明

--- README.md
+++ README.md
@@ -27,3 +27,44 @@ npm run dev
 ```sh
 npm run build
 ```
+export default defineConfig({
+	// 指定部署後的基路徑
+	base: "./",
+
+	//  Vite 提供許多官方插件
+	plugins: [
+		vue(),
+		legacy({
+			targets: ["defaults", "not IE 11"],
+		}),
+	],
+
+	server: {
+		port: 3000, // 配置開發服務器的端口為 3000
+    },
+
+	// 配置模組解析。通過 alias 屬性指定路徑別名，之後方便在 Code 中引用模塊路徑時簡化路徑。
+	resolve: {
+		alias: {
+			alias: {
+				// 可以確保路徑解析是安全和一致的，特別是在需要在不同操作系統下運行的多平台專案中。
+				"@": fileURLToPath(new URL("./src", import.meta.url)),
+				// "@": "/src"
+			},
+		},
+	},
+	build: {
+		// 該配置用於指定文件大小的警告閾值，當打包生成的文件大小超過這個值時，會發出警告提醒開發者注意。
+		chunkSizeWarningLimit: 1500,
+		// // 意味著小於 4KB 的資源將被轉換為 base64 格式並內嵌到 JavaScript / CSS 中。這樣做的好處是減少了 HTTP 請求的數量，提高了頁面的加載速度。
+		assetsInlineLimit: 4096,
+		// rollupOptions: {
+		// 	input: "src/main.js",
+		// 	output: {
+		// 		entryFileNames: "bundle.js",
+		// 		chunkFileNames: "[name]-[hash].js",
+		// 		assetFileNames: "assets/[name]-[hash].[ext]",
+		// 	},
+		// },
+	},
+});
--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
 	// 指定部署後的基路徑
 	base: "./",
 
-	//  Vite 提供許多官方插件，例如@vitejs/plugin-vue 和 vite-plugin-eslint，前者用於 Vue 組件，後者用於 ESLint 代碼檢查工具。
+	//  Vite 提供許多官方插件
 	plugins: [
 		vue(),
 		legacy({
@@ -17,31 +17,32 @@ export default defineConfig({
 		}),
 	],
 
+	server: {
+		port: 3000, // 配置開發服務器的端口為 3000
+    },
+
 	// 配置模組解析。通過 alias 屬性指定路徑別名，之後方便在 Code 中引用模塊路徑時簡化路徑。
 	resolve: {
 		alias: {
-			"@": fileURLToPath(new URL("./src", import.meta.url)),
+			alias: {
+				// 可以確保路徑解析是安全和一致的，特別是在需要在不同操作系統下運行的多平台專案中。
+				"@": fileURLToPath(new URL("./src", import.meta.url)),
+				// "@": "/src"
+			},
 		},
 	},
-
-	// 配置構建。如通過 chunkSizeWarningLimit 屬性設定 chunk 大小的警告閾值。
 	build: {
 		// 該配置用於指定文件大小的警告閾值，當打包生成的文件大小超過這個值時，會發出警告提醒開發者注意。
 		chunkSizeWarningLimit: 1500,
-
-		// 意味著小於 4KB 的資源將被轉換為 base64 格式並內嵌到 JavaScript / CSS 中。這樣做的好處是減少了 HTTP 請求的數量，提高了頁面的加載速度。
+		// // 意味著小於 4KB 的資源將被轉換為 base64 格式並內嵌到 JavaScript / CSS 中。這樣做的好處是減少了 HTTP 請求的數量，提高了頁面的加載速度。
 		assetsInlineLimit: 4096,
-
-    // 可以進行各種自定義的 Rollup 配置。
-    rollupOptions: {
-      output: {
-          // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
-            sanitizeFileName(name) {
-              const match = DRIVE_LETTER_REGEX.exec(name);
-              const driveLetter = match ? match[0] : '';
-              return driveLetter + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '');
-            }
-      }
-    }
+		// rollupOptions: {
+		// 	input: "src/main.js",
+		// 	output: {
+		// 		entryFileNames: "bundle.js",
+		// 		chunkFileNames: "[name]-[hash].js",
+		// 		assetFileNames: "assets/[name]-[hash].[ext]",
+		// 	},
+		// },
 	},
 });
